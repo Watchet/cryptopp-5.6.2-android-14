6a3841252530cfbbc27fcda05af5088d87b684c4
Jeffrey Walton <noloader@gmail.com>
2014-09-01 01:48:36 -0400

    Updated files

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-Crypto++ 5.6.2 (revision 541) built for Android-14 API, ARMv7 and STLport using the R8e NDK and arm-linux-androideabi-4.6 toolchain. The ZIP includes the headers, libcryptopp.so and libcryptopp.a.
+Crypto++ 5.6.2 (revision 541) built for Android-14 API, ARMv7 and STLport using the R9 NDK and arm-linux-androideabi-4.8 toolchain. The ZIP includes the headers, libcryptopp.so and libcryptopp.a.
 
 The library use STLport because the STLport has a permissive, BSD style license. If you switch to GNU libstdc++, then you might encounter uncomfortable licensing terms. 
 
 If you only want to include, compile, and link, then only download cryptopp-5.6.2-android-14.zip. It has everything you need for an Eclipse or command line project. Unzip the ZIP archive and place it in a convenient location. Use unzip -a to ensure CRLF are handled properly. /usr/local/cryptopp-android-14/ is a good location since it is world readable and write protected.
 
-The additional files provided in the set are used to update the standard Crypto++ distribution and build the libcryptopp.so and libcryptopp.a libraries. Place them in the same directory as Crypto++ source files (overwriting config.h and GNUmakefile as necessary), and then run `. ./setenv-android.sh` (note the leading dot to ensure the changes affect the current shell). Then run `make static dynamic` as usual. If you don't care about building libcryptopp.so or libcryptopp.a yourself, then ignore the additional files.
+The additional files provided in cryptopp-mobile.zip are used to update the standard Crypto++ distribution and build the libcryptopp.so and libcryptopp.a libraries for Android. Place them in the same directory as Crypto++ source files (overwriting the original files). With the files in place, then run `. ./setenv-android.sh` (note the leading dot to ensure the changes affect the current shell). Then run `make static dynamic` as usual. If you don't care about building libcryptopp.so or libcryptopp.a yourself, then ignore the additional files.
+
+When you create your Java class that uses your native library, be sure to:
+
+    static {
+		System.loadLibrary("stlport_shared");
+		System.loadLibrary("cryptopp");
+        System.loadLibrary("mylibrary");
+	}
+	
+When you build your APK, be sure to place `libstlport_shared.so`, `libcryptopp.so` and `libmylibrary.so` in the `<project dir>/libs/armeabi/` folder. Also be sure to link your `libmylibrary.so` with `libstlport_shared.so`.
 
 See http://www.cryptopp.com/wiki/Android_(Command_Line) for details on how the environment was set, how the library was built and how to use the library in an Eclipse project. If you have questions, then ask on the Crypto++ Users group at https://groups.google.com/forum/#!forum/cryptopp-users.
--- setenv-android.sh
+++ setenv-android.sh
@@ -16,22 +16,70 @@
 # login script. If ANDROID_NDK_ROOT is not specified, the script will
 # try to pick it up with the value of _ANDROID_NDK_ROOT below. If
 # ANDROID_NDK_ROOT is set, then the value is ignored.
-_ANDROID_NDK="android-ndk-r8e"
+# _ANDROID_NDK="android-ndk-r8"
+_ANDROID_NDK="android-ndk-r9"
+# _ANDROID_NDK="android-ndk-r10"
 
 # Set _ANDROID_EABI to the EABI you want to use. You can find the
 # list in $ANDROID_NDK_ROOT/toolchains. This value is always used.
-_ANDROID_EABI="arm-linux-androideabi-4.6"
+# _ANDROID_EABI="mipsel-linux-android-4.8"
+# _ANDROID_EABI="x86-4.6"
+# _ANDROID_EABI="arm-linux-androideabi-4.6"
+_ANDROID_EABI="arm-linux-androideabi-4.8"
+
+# Set _ANDROID_ARCH to the architecture you are building for.
+# This value is always used.
+# _ANDROID_ARCH=arch-mips
+# _ANDROID_ARCH=arch-x86
+_ANDROID_ARCH=arch-arm
 
 # Set _ANDROID_API to the API you want to use. You should set it
-# to one of: android-14, android-9, android-8, android-14, android-5
-# android-4, or android-3. You can't set it to the latest (for
-# example, API-17) because the NDK does not supply the platform. At
-# Android 5.0, there will likely be another platform added (android-18?).
-# This value is always used.
+# to one of: android-19, android-18, android-14, android-9, android-8,
+# android-5, android-4, or android-3. You should set it to the minimum
+# SDK you want to support, not the latest. This value is always used.
+# _ANDROID_API="android-9"
 _ANDROID_API="android-14"
+# _ANDROID_API="android-18"
+# _ANDROID_API="android-19"
 
 # For Android 4.3 (released July 2013), the tuple is { android-ndk-r9,
-# arm-linux-androideabi-4.8, android-18 }.
+# arm-linux-androideabi-4.8, arch-arm, android-18 }.
+
+#####################################################################
+
+# The Crypto++ makefile uses CXX and LD to determine the host capabilities.
+# We need to set them early because IS_ANDROID is used after some
+# capabilities are determined. We set the others (like AS and RANLIB)
+# because this script is useful for other projects, including Autotools.
+
+if [ "$_ANDROID_ARCH" == "arch-arm" ]; then
+	export CPP="arm-linux-androideabi-cpp"
+	export CC="arm-linux-androideabi-gcc"
+	export CXX="arm-linux-androideabi-g++"
+	export LD="arm-linux-androideabi-ld"
+	export AS="arm-linux-androideabi-as"
+	export AR="arm-linux-androideabi-ar"
+	export RANLIB="arm-linux-androideabi-ranlib"
+	export STRIP="arm-linux-androideabi-strip"
+elif [ "$_ANDROID_ARCH" == "arch-x86" ]; then
+	export CPP="i686-linux-android-cpp"
+	export CC="i686-linux-android-gcc"
+	export CXX="i686-linux-android-g++"
+	export LD="i686-linux-android-ld"
+	export AS="i686-linux-android-as"
+	export AR="i686-linux-android-ar"
+	export RANLIB="i686-linux-android-ranlib"
+	export STRIP="i686-linux-android-strip"
+elif [ "$_ANDROID_ARCH" == "arch-mips" ]; then
+	export CPP="mipsel-linux-android-cpp"
+	export CC="mipsel-linux-android-gcc"
+	export CXX="mipsel-linux-android-g++"
+	export LD="mipsel-linux-android-ld"
+	export AS="mipsel-linux-android-as"
+	export AR="mipsel-linux-android-ar"
+	export RANLIB="mipsel-linux-android-ranlib"
+	export STRIP="mipsel-linux-android-strip"
+fi
 
 #####################################################################
 
@@ -108,42 +156,42 @@ if [ -z "$ANDROID_TOOLCHAIN" ] || [ ! -d "$ANDROID_TOOLCHAIN" ]; then
 fi
 
 # Error checking
-if [ ! -e "$ANDROID_TOOLCHAIN/arm-linux-androideabi-cpp" ]; then
+if [ ! -e "$ANDROID_TOOLCHAIN/$CPP" ]; then
   echo "Error: Failed to find Android cpp. Please edit this script."
   # exit 1
 fi
 
 # Error checking
-if [ ! -e "$ANDROID_TOOLCHAIN/arm-linux-androideabi-gcc" ]; then
+if [ ! -e "$ANDROID_TOOLCHAIN/$CC" ]; then
   echo "Error: Failed to find Android gcc. Please edit this script."
   # exit 1
 fi
 
-if [ ! -e "$ANDROID_TOOLCHAIN/arm-linux-androideabi-g++" ]; then
+if [ ! -e "$ANDROID_TOOLCHAIN/$CXX" ]; then
   echo "Error: Failed to find Android g++. Please edit this script."
   # exit 1
 fi
 
 # Error checking
-if [ ! -e "$ANDROID_TOOLCHAIN/arm-linux-androideabi-ranlib" ]; then
+if [ ! -e "$ANDROID_TOOLCHAIN/$RANLIB" ]; then
   echo "Error: Failed to find Android ranlib. Please edit this script."
   # exit 1
 fi
 
 # Error checking
-if [ ! -e "$ANDROID_TOOLCHAIN/arm-linux-androideabi-ar" ]; then
+if [ ! -e "$ANDROID_TOOLCHAIN/$AR" ]; then
   echo "Error: Failed to find Android ar. Please edit this script."
 # exit 1
 fi
 
 # Error checking
-if [ ! -e "$ANDROID_TOOLCHAIN/arm-linux-androideabi-as" ]; then
+if [ ! -e "$ANDROID_TOOLCHAIN/$AS" ]; then
   echo "Error: Failed to find Android as. Please edit this script."
 # exit 1
 fi
 
 # Error checking
-if [ ! -e "$ANDROID_TOOLCHAIN/arm-linux-androideabi-ld" ]; then
+if [ ! -e "$ANDROID_TOOLCHAIN/$LD" ]; then
   echo "Error: Failed to find Android ld. Please edit this script."
   # exit 1
 fi
@@ -164,11 +212,11 @@ fi
 # For the Android SYSROOT. Can be used on the command line with --sysroot
 # https://android.googlesource.com/platform/ndk/+/ics-mr0/docs/STANDALONE-TOOLCHAIN.html
 ANDROID_API="$_ANDROID_API"
-export ANDROID_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_API/arch-arm"
+export ANDROID_SYSROOT="$ANDROID_NDK_ROOT/platforms/$ANDROID_API/$_ANDROID_ARCH"
 
 # Error checking
 if [ -z "$ANDROID_SYSROOT" ] || [ ! -d "$ANDROID_SYSROOT" ]; then
-  echo "Error: ANDROID_SYSROOT is not valid. Please edit this script."
+  echo "Error: ANDROID_SYSROOT is not valid. Does the NDK support the API? Please edit this script."
   # exit 1
 fi
 
@@ -182,10 +230,10 @@ if [ ! -d "$ANDROID_NDK_ROOT/sources/cxx-stl/stlport/stlport/" ]; then
   # exit 1
 fi
 
-# If more than one library is using STLport, all libraries ***must*** use the shared version
+# If more than one library is using STLport, then all libraries in the
+# APK ***must*** use the shared version.
 # STLPORT_LIB=libstlport_static.a
 STLPORT_LIB=libstlport_shared.so
-
 
 # Error checking
 if [ ! -e "$ANDROID_NDK_ROOT/sources/cxx-stl/stlport/libs/armeabi/$STLPORT_LIB" ]; then
@@ -203,23 +251,12 @@ export ANDROID_STL_LIB="$ANDROID_NDK_ROOT/sources/cxx-stl/stlport/libs/armeabi/$
 export IS_ANDROID=1
 export IS_CROSS_COMPILE=1
 
-# The Crypto++ makefile uses CXX and LD to determine the host capabilities.
-# We need to set them early because IS_ANDROID is used after some
-# capabilities are determined. We set the others (like AS and RANLIB because
-# this script is useful for other projects, including Autotools.
-export CPP=arm-linux-androideabi-cpp
-export CC=arm-linux-androideabi-gcc
-export CXX=arm-linux-androideabi-g++
-export LD=arm-linux-androideabi-ld
-export AS=arm-linux-androideabi-as
-export AR=arm-linux-androideabi-ar
-export RANLIB=arm-linux-androideabi-ranlib
-
 VERBOSE=1
 if [ ! -z "$VERBOSE" ] && [ "$VERBOSE" != "0" ]; then
   echo "ANDROID_NDK_ROOT: $ANDROID_NDK_ROOT"
   echo "ANDROID_EABI: $_ANDROID_EABI"
-  echo "ANDROID_API: $ANDROID_API"
+  echo "ANDROID_ARCH: $_ANDROID_ARCH"
+  echo "ANDROID_API: $_ANDROID_API"
   echo "ANDROID_SYSROOT: $ANDROID_SYSROOT"
   echo "ANDROID_TOOLCHAIN: $ANDROID_TOOLCHAIN"
   echo "ANDROID_STL_INC: $ANDROID_STL_INC"
